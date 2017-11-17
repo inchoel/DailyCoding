9da9933ac57a5a0144c46a724118f9008c495d7b
ic.lee <inchoel.lee@gmail.com>
2017-11-17 19:05:07 +0900

    acmicpc.net/1963_소수경로.cpp - BFS 구현중...
    :Detailed Notes:
    버그도 있고, 최적화할 필요가 있음...

--- acmicpc.net/1963_소수경로.cpp
+++ acmicpc.net/1963_소수경로.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <algorithm>
 #include <vector>
+#include <queue>
 
 #include <thread>
 #include <chrono>
@@ -34,7 +35,10 @@ void findPrime (vector<int>& v) {
 }
 
 int getStepToMove(const int cur, const int target) {
-  int step = 0, pos = 0;
+  findPrime(prime);
+  prime_size = prime.size();
+
+  int pos = 0;
   for (int i=0; i<prime_size; i++) {
     if (prime[i] == cur) {
       pos = i + 1;
@@ -46,46 +50,53 @@ int getStepToMove(const int cur, const int target) {
   q.push(cur);
   vector<int> curV(4), nextV(4);
 
-  int num, equal;
+  int num, equal, q_size, step=0;
   while (!q.empty()) {
-    num = q.front();
-    q.pop();
+    q_size = q.size();
+    step++;
+    // printf("qsize=%d, step=%d\n", q_size, step);
+    while (q_size--) {
+      num = q.front();
+      q.pop();
 
-    curV[0] = num/1000;
-    curV[1] = num%1000/100;
-    curV[2] = num%100/10;
-    curV[3] = num%10;
+      curV[0] = num/1000;
+      curV[1] = num%1000/100;
+      curV[2] = num%100/10;
+      curV[3] = num%10;
 
-    for (int i=pos; i<prime_size; i++) {
-      nextV[0] = prime[i]/1000;
-      nextV[1] = prime[i]%1000/100;
-      nextV[2] = prime[i]%100/10;
-      nextV[3] = prime[i]%10;
+      for (int i=pos; i<prime_size; i++) {
+        if (prime[i] == 0)
+          continue;
 
-      equal = 0;
-      for (int j=0; j<4; j++) {
-        if (curV[j] == nextV[j])
-          equal++; 
-      }
+        nextV[0] = prime[i]/1000;
+        nextV[1] = prime[i]%1000/100;
+        nextV[2] = prime[i]%100/10;
+        nextV[3] = prime[i]%10;
 
-      if (equal >= 3) {
-        q.push(prime[i]);
-        printf("prime=%d, target=%d\n", prime[i], target);
-        this_thread::sleep_for(chrono::milliseconds(300));
+        equal = 0;
+        for (int j=0; j<4; j++) {
+          if (curV[j] == nextV[j])
+            equal++; 
+        }
 
-        if (prime[i] == new_pw)
-          return step;
+        if (equal >= 3) {
+          q.push(prime[i]);
 
-        curV = nextV;
+          // printf("%d ", prime[i]);
+
+          if (prime[i] == target)
+            return step;
+
+          prime[i] = 0;
+        }
       }
     }
+    // printf("\n");
+    // this_thread::sleep_for(chrono::milliseconds(300));
   }
 }
 
 int main() {
-  findPrime(prime);
-  prime_size = prime.size();
-
   int t, old_pw, new_pw;
   scanf (" %d", &t);
 
