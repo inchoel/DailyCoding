696bea092d914c6cebfa51a5eb62e6bd2f2463ff
ic.lee <inchoel.lee@gmail.com>
2017-12-13 08:58:55 +0900

    acmicpc.net/1759_암호만들기.cpp - next_permutation사용했는데 시간초과

--- acmicpc.net/1759_암호만들기.cpp
+++ acmicpc.net/1759_암호만들기.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <algorithm>
-#include <utility>
 #include <vector>
+#include <set>
 using namespace std;
 
 static vector<int> v = {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
@@ -14,11 +14,17 @@ int main() {
   for (int i=0; i<c; i++)
     scanf (" %c", &crypto[i]);
 
+  sort (crypto.begin(), crypto.end());
   int vowel, i;
   char ans[5] = {0, };
+  set<string> ans_set;
   do {
+
     vowel = 0;
     ans[0] = crypto[0];
+    if (v[crypto[0]-'a'])
+      vowel++;
+
     for (i=1; i<4; i++) {
       if (crypto[i-1] > crypto[i])
         break;
@@ -29,10 +35,13 @@ int main() {
       ans[i] = crypto[i];
     }
 
-    if (i==4 && (vowel>0 && vowel<3))
-      printf("%s\n", ans);
+    if (i==4 && (vowel>0 && vowel<3)) {
+      ans_set.emplace(ans);
+    }
 
   } while (next_permutation(crypto.begin(), crypto.end()));
 
+  for (auto a : ans_set)
+    printf("%s\n", a.c_str());
   return 0;
 }
