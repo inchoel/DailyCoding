8554e2d094fc0db323d86a7b8b5ca56d13c59a23
ic.lee <inchoel.lee@gmail.com>
2017-12-14 08:25:41 +0900

    unknow/assign_null_to_string.cpp - 사례추가

--- unknow/assign_null_to_string.cpp
+++ unknow/assign_null_to_string.cpp
@@ -2,10 +2,17 @@
 #include <string>
 using namespace std;
 
+const char* foo() {
+  // return NULL;
+  return 0;
+}
+
 int main() {
   string str = "xxx";
   printf("%s\n", str.c_str());
-  str = nullptr;
-  // printf("%d\n", str.empty());
+  // str = 0; // compile error
+  // str = nullptr; // runtime error
+  //
+  str = foo();
   return 0;
 }
