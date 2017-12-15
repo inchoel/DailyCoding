faf50c3011e24127843a115648f38fc048dad2c4
ic.lee <inchoel.lee@gmail.com>
2017-12-15 09:47:06 +0900

    ./acmicpc.net/1759_암호만들기.cpp - DFS로 가닥을 잡았는데, 리커시브.. 어렵다. 연마를 더 해야 할 듯...

--- acmicpc.net/1759_암호만들기.cpp
+++ acmicpc.net/1759_암호만들기.cpp
@@ -5,29 +5,40 @@
 using namespace std;
 
 static vector<int> v = {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
-int l, c;
+int L, C;
+string lock(5);
 
-void bfs (int i, vector<char>& c, vector<bool>& visited) {
-  do {
-    lock[lock_i] = crypto[i];
-    lock_i++;
-  } while (lock_i<4);
+void generate_lock (size_t l_i, size_t c_i, vector<char>& crypto, size_t vowel) {
+  if (vowel > 2) {
+    return;
+  }
+
+  if (l_i == 4) {
+    if (vowel > 0)
+      printf("%s\n", lock.c_str());
+
+    return;
+  }
+
+  lock[l_i] = crypto[c_i];
+
+  for (int i=c_i; i<C-3; ++) {
+    if (v[crypto[i]-'a'])
+      generate_lock (l_i+1, i, crypto, vowel+1);
+    else
+      generate_lock (l_i+1, i, crypto, vowel);
+  }
 }
 
 int main() {
-  scanf (" %d %d", &l, &c);
+  scanf (" %d %d", &L, &C);
 
   vector<char> crypto(c);
-  for (int i=0; i<c; i++)
+  for (int i=0; i<C; i++)
     scanf (" %c", &crypto[i]);
 
   sort (crypto.begin(), crypto.end());
 
-  vector<bool> isVisited(c, false);
-
-  for (int i=0; i<4; i++) {
-    bfs (i, crypto, isVisited);
-  }
-
+  generate_lock (0, 0, crypto, 0);
   return 0;
 }
