512e2752742c95b0c096ddc8fe754c664ef4684a
ic.lee <inchoel.lee@gmail.com>
2017-12-19 17:20:15 +0900

    ./acmicpc.net/1759_암호만들기.cpp - DFS 해결, backtracking 등으로 응용하려면 많은 연습을 해야 할 듯하다..

--- acmicpc.net/1759_암호만들기.cpp
+++ acmicpc.net/1759_암호만들기.cpp
@@ -1,44 +1,40 @@
 #include <cstdio>
 #include <algorithm>
 #include <vector>
-#include <set>
+#include <string>
 using namespace std;
 
 static vector<int> v = {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
-int L, C;
-string lock(5);
+size_t L, C;
 
-void generate_lock (size_t l_i, size_t c_i, vector<char>& crypto, size_t vowel) {
-  if (vowel > 2) {
+void generate_lock (size_t c_i, string lock, vector<char>& crypto, size_t vowel, size_t consonent) {
+  if (lock.size() > L)
     return;
+
+  if (lock.size() == L) {
+    if (vowel < 1 || consonent < 2)
+      return;
+
+    printf("%s\n", lock.c_str());
   }
 
-  if (l_i == 4) {
-    if (vowel > 0)
-      printf("%s\n", lock.c_str());
-
-    return;
-  }
-
-  lock[l_i] = crypto[c_i];
-
-  for (int i=c_i; i<C-3; ++) {
+  for (size_t i=c_i; i<C; ++i) {
     if (v[crypto[i]-'a'])
-      generate_lock (l_i+1, i, crypto, vowel+1);
+      generate_lock (i+1, lock+crypto[i], crypto, vowel+1, consonent);
     else
-      generate_lock (l_i+1, i, crypto, vowel);
+      generate_lock (i+1, lock+crypto[i], crypto, vowel, consonent+1);
   }
 }
 
 int main() {
-  scanf (" %d %d", &L, &C);
+  scanf (" %lu %lu", &L, &C);
 
-  vector<char> crypto(c);
-  for (int i=0; i<C; i++)
+  vector<char> crypto(C);
+  for (size_t i=0; i<C; i++)
     scanf (" %c", &crypto[i]);
 
   sort (crypto.begin(), crypto.end());
 
-  generate_lock (0, 0, crypto, 0);
+  generate_lock (0, "", crypto, 0, 0);
   return 0;
 }
