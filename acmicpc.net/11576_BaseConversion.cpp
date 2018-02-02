c4e0c3b93ec41884d5350dea83aa389b82385d7b
ic.lee <inchoel.lee@gmail.com>
2018-02-02 13:19:42 +0900

    acmicpc.net/11576_BaseConversion.cpp - 입력부 구현

---
+++ acmicpc.net/11576_BaseConversion.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+int main() {
+  unsigned long int a, b;
+  scanf (" %d %d", &a, &b);
+
+  int m;
+  scanf (" %d", &m);
+
+  int posNum[26];
+  for (int i=0; i<m; i++) {
+    scanf (" %d", &posNum[i]);
+  }
+
+
+  return 0;
+}
