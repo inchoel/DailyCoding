393518854c0ebed3f2bfe623d507683699aebd8f
ic.lee <inchoel.lee@gmail.com>
2018-02-05 08:10:06 +0900

    acmicpc.net/11576_BaseConversion.cpp - clear

--- acmicpc.net/11576_BaseConversion.cpp
+++ acmicpc.net/11576_BaseConversion.cpp
@@ -1,19 +1,37 @@
 #include <cstdio>
-#include <vector>
+#include <cmath>
+#include <stack>
 using namespace std;
 
+void converter (unsigned long long int decimal, int base, stack<int> &s) {
+  do {
+    s.push(decimal % base);
+    decimal = decimal / base;
+  } while (decimal != 0);
+}
+
 int main() {
-  unsigned long int a, b;
+  int a, b;
   scanf (" %d %d", &a, &b);
 
   int m;
   scanf (" %d", &m);
 
+  unsigned long long int decimal=0;
   int posNum[26];
-  for (int i=0; i<m; i++) {
+  for (int i=1; i<=m; i++) {
     scanf (" %d", &posNum[i]);
+    decimal += (posNum[i] * pow(a, m-i));
   }
 
+  stack<int> s;
+  converter (decimal, b, s);
+
+  while (!s.empty()) {
+    printf("%d ", s.top());
+    s.pop();
+  }
+  printf ("\n");
 
   return 0;
 }
