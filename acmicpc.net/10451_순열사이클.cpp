95c9e82e0ce276b2eb37a211bdf992feccad3e38
ic.lee <inchoel.lee@gmail.com>
2018-03-12 09:00:21 +0900

    Update acmicpc.net/10451_순열사이클.cpp

---
+++ acmicpc.net/10451_순열사이클.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+int main() {
+  int t, n;
+  scanf (" %d", &t);
+  while (t--) {
+    scanf (" %d", &n);
+    vector<int> v(n);
+    for (int i=0; i<n; i++) {
+      scanf (" %d", &v[i]);
+    }
+
+  }
+
+  return 0;
+}
