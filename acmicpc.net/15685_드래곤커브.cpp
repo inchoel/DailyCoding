e12080b506041bf897667fb34edd4986dfa0bcc7
ic.lee <ic.lee@lge.com>
2018-07-17 08:56:41 +0900

    acmicpc.net/15685_드래곤커브.cpp - 단순구현인데 규칙찾기가 어렵다.

--- acmicpc.net/15685_드래곤커브.cpp
+++ acmicpc.net/15685_드래곤커브.cpp
@@ -4,25 +4,64 @@
 #include <vector>
 using namespace std;
 
-bool map[100][100];
+bool map[102][102];
+int dx[4] = {0, -1, 0, 1};
+int dy[4] = {1, 0, -1, 0};
 
-void drawDragonCurve(const vector<int>& v)
-{
-  printf("(%d, %d), direction:%d, generation:%d\n", v[0], v[1], v[2], v[3]);
+int end_x = 0;
+int end_y = 0;
+
+vector<int> dragon;
+
+
+void drawDragon(vector<int>& dragon) {
+  int size = (int)dragon.size();
+
+  for (int i=size-1; i>=0; i--) {
+    int dir = (dragon[i]+1)%4;
+
+    end_x = end_x + dx[dir];
+    end_y = end_y + dy[dir];
+
+    map[end_x][end_y] = true;
+
+    dragon.push_back(dir);
+  }
 }
 
-int main() {
+int main(void) {
   int n;
   scanf (" %d", &n);
 
-  vector<int> d(4);
   for (int i=0; i<n; i++) {
-    scanf (" %d %d %d %d", &d[0], &d[1], &d[2], &d[3]);
-    // 1. Draw dragon curve on the map
-    drawDragonCurve(d);
+    int x, y, d, g;
+    // scanf (" %d %d %d %d", &x, &y, &d, &g);
+    scanf (" %d %d %d %d", &y, &x, &d, &g);
+    end_x = x;
+    end_y = y;
+    map[end_x][end_y] = true;
+    end_x = x + dx[d];
+    end_y = y + dy[d];
+    map[end_x][end_y] = true;
+    dragon.clear();
+    dragon.push_back(d);
+
+    for (int i=0; i<g; i++)
+      drawDragon(dragon);
   }
 
-  // 2. Check 4 vertex
+  int ans = 0;
+  for (int i=0; i<=100; i++) {
+    for (int j=0; j<=100; j++) {
+      if (map[i][j]==true &&
+          map[i][j+1]==true &&
+          map[i+1][j]==true &&
+          map[i+1][j+1]==true)
+        ans++;
+    }
+  }
+
+  printf("%d\n", ans);
 
   return 0;
 }
