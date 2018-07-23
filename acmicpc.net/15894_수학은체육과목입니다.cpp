9c726d31f16d4f97c3f52080c6507d99a859a5d3
ic.lee <ic.lee@lge.com>
2018-07-24 07:56:28 +0900

    acmicpc.net/15894_수학은체육과목입니다.cpp - 단순하지만 놓치기 쉬운문제

---
+++ acmicpc.net/15894_수학은체육과목입니다.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+int main() {
+  long long unsigned int n;
+  scanf (" %llu", &n);
+
+  printf("%llu\n", n*4);
+  return 0;
+}
